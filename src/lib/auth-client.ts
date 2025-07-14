32ecaa1840e6deb87c48873fbf100ee96469a2d1
yosho <yleecruz@gmail.com>
2025-07-14 16:10:52 -0400

    all working but not cookies

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -10,5 +10,11 @@ export const authClient = createAuthClient({
 	},
 	fetchOptions: {
 		credentials: "include",
+		headers: {
+			"Access-Control-Allow-Origin": "http://localhost:5173",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			"Access-Control-Allow-Credentials": "true",
+		},
 	},
 });
