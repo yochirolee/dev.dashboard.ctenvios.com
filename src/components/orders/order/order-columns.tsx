8e1a1ed113de881ecd86584f23c5890c93af92be
yosho <yleecruz@gmail.com>
2025-08-12 11:41:27 -0400

    pr some media queries 33

--- src/components/orders/order/order-columns.tsx
+++ src/components/orders/order/order-columns.tsx
@@ -22,8 +22,7 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Link } from "react-router-dom";
-import { queryClient } from "@/lib/query-client";
-import api from "@/api/api";
+
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
