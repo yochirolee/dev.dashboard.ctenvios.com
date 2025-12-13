cd5b0c17e7ec4670561d7aff95f14f8b1fc9dbbd
Yosho <yleecruz@gmail.com>
2025-12-13 12:10:42 -0500

    Feature: Added AppLogs2

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -19,8 +19,8 @@ import { HomePage } from "@/pages/home-page";
 import { CreateDispatchPage } from "@/pages/logistics/Dispatch/create-dispatch-page";
 import { ReceiveDispatchPage } from "@/pages/logistics/Dispatch/receive-dispatch-page";
 import { DispatchPageLists } from "@/pages/logistics/dispatch-page-list";
-import AppLogsPage from "../pages/logs/app-logs-page";
-import PartnersLogsPage from "../pages/logs/partners-logs-page";
+import AppLogsPage from "@/pages/logs/app-logs-page";
+import PartnersLogsPage from "@/pages/logs/partners-logs-page";
 
 export const AppRouter = () => {
    return (
